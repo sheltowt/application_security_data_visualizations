ae14d8f699ff9335e7ccd4e00459e275f2c81f5f
new_account <sheltowt@gmail.com>
2020-05-24 12:37:20 -0600

    adding updates to data visualizations

--- web_app/app.js
+++ web_app/app.js
@@ -5,8 +5,16 @@ app.get('/', function(req, res) {
 	res.send('hello world');
 })
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/sca', function(req, res) {
+    res.sendFile('./sca_visualization/index.html', { root: __dirname });
+});
+
+app.get('/sast', function(req, res) {
+    res.sendFile('./sast_visualization/index.html', { root: __dirname });
+});
+
+app.get('/dast', function(req, res) {
+    res.sendFile('./dast_visualization/index.html', { root: __dirname });
 });
 
 app.listen(3000, function(){
