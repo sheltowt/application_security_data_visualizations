2f77ff69a77a2b503c3ad735076878148b3e2d59
new_account <sheltowt@gmail.com>
2020-05-24 12:26:06 -0600

    adding mvp of visualizations

--- web_app/app.js
+++ web_app/app.js
@@ -5,6 +5,10 @@ app.get('/', function(req, res) {
 	res.send('hello world');
 })
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 app.listen(3000, function(){
 	console.log('App listening on port 3000');
 })
