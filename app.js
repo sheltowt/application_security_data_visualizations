3c0489e9e6c93bfecd58fb28cbc69e07ce782305
new_account <sheltowt@gmail.com>
2020-06-15 10:24:53 -0600

    adding web app updates

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.use(express.static('cloud_security_audit/ScoutSuite/scoutsuite-report'));
 
 app.get('/', function(req, res) {
-	res.send('hello world');
-})
+    res.sendFile('./network_visualization/index.html', { root: __dirname });
+});
+
+app.get('/cloudappalerts', function(req, res) {
+    res.sendFile('./alerts/cloud_application_incidents.html', { root: __dirname });
+});
 
 app.get('/sca', function(req, res) {
     res.sendFile('./sca_visualization/index.html', { root: __dirname });
@@ -17,6 +22,22 @@ app.get('/sast', function(req, res) {
 
 app.get('/dast', function(req, res) {
     res.sendFile('./dast_visualization/index.html', { root: __dirname });
+});
+
+app.get('/audit', function(req, res) {
+    res.sendFile('./cloud_security_audit/ScoutSuite/scoutsuite-report/aws-143762891182.html', { root: __dirname });
+});
+
+app.get('/nginx', function(req, res) {
+    res.sendFile('./elasticsearch_visualizations/nginx.html', { root: __dirname });
+});
+
+app.get('/osquery', function(req, res) {
+    res.sendFile('./elasticsearch_visualizations/osquery.html', { root: __dirname });
+});
+
+app.get('/securityhub', function(req, res) {
+    res.sendFile('./elasticsearch_visualizations/security_hub.html', { root: __dirname });
 });
 
 app.listen(3000, function(){
